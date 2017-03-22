894892c8f38511a2c7ba3b30660b73da3a44d92c
Tim Tregubov <tim@zingweb.com>
2017-03-22 12:30:08 -0400

    updating leaflet and playing with markers

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ var map = L.map('map',{
     tms: false
 }).setView([42.755942, -72.8092041],3);
 
+// map.on('zoomend', function() {
+//   var currentZoom = map.getZoom();
+//   facemarkers.eachLayer(function(marker) {
+//     $(marker._icon).height(currentZoom * 20);
+//     $(marker._icon).width(currentZoom * 20);
+//   });
+// });
+
 var PersonIcon = L.Icon.extend({
     options: {
         iconSize: [
@@ -17,13 +25,28 @@ var PersonIcon = L.Icon.extend({
 var Stamen_Watercolor = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', {}).addTo(map);
 
 // all the facemarkers will go into one layer
+// var facemarkers = L.markerClusterGroup({
+  // animate: true,
+  // spiderfyDistanceMultiplier: 10.0,
+  // maxClusterRadius: 50,
+
+  // spiderLegPolylineOptions: {weight: 0},
+  // clockHelpingCircleOptions: {weight: .7, opacity: 1, color: 'black', fillOpacity: 0, dashArray: '10 5'},
+  // elementsPlacementStrategy: 'clock',
+  // helpingCircles: true,
+  // spiderfyDistanceSurplus: 25,
+  // spiderfyDistanceMultiplier: 1,
+  // elementsMultiplier: 1.4,
+  // firstCircleElements: 8
+// });
+
 var facemarkers = L.layerGroup();
 
 // function to add markers
 var addMarker = function(options) {
-  var icon = new PersonIcon({iconUrl: options.iconUrl});
-  var marker = L.marker(options.lat_long, {icon: icon, url: options.url, name: options.name, tags: [options.year]});
-  facemarkers.addLayer(marker.addTo(map).bindPopup(options.message));
+  var icon = new PersonIcon({iconUrl: 'http://mappy.dali.dartmouth.edu/' + options.iconUrl});
+  var marker = L.marker(options.lat_long, {icon: icon, url:  options.url, name: options.name, tags: [options.year]}).bindPopup(options.message);
+  facemarkers.addLayer(marker);
 };
 
 
@@ -32,6 +55,7 @@ var loadJSON = function(callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
     xobj.open('GET', 'people.json');
+    // xobj.open('GET', 'https://raw.githubusercontent.com/dali-lab/mappy/gh-pages/members.json');
     xobj.onreadystatechange = function () {
       if (xobj.readyState == 4 && xobj.status == "200") {
         callback(xobj.responseText);
@@ -55,7 +79,6 @@ loadJSON(function(response) {
       filterOnEveryClick: 'true'
   }).addTo(map);
 
-  // add all markers to a layer
   facemarkers.eachLayer(function(marker) {
     marker.on('mouseover', function (e) {
       e.target.openPopup();
@@ -67,6 +90,9 @@ loadJSON(function(response) {
       eModal.iframe({url: e.target.options.url, title: e.target.options.name, size: 'lg'});
     });
   });
+
+  map.addLayer(facemarkers);
+  // facemarkers.refreshClusters();
 
 });
 
